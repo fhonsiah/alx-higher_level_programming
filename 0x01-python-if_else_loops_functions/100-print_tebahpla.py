83796d796e69911452d15d1907309924768f7973
fhonsiah <fhonsiah@gmail.com>
2023-01-23 01:41:44 -0800

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+alphabet = ""
 for i in range(ord('z'), ord('a')-1, -1):
-    print(chr(i) if i % 2 else chr(i-32), end='')
+    alphabet = "{}{}".format(alphabet, chr(i) if i % 2 else chr(i-32))
+print(alphabet)
