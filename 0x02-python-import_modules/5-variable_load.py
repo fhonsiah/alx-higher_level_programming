6c1a0a52967742aff1cdba7aab12fa6556829b11
fhonsiah <fhonsiah@gmail.com>
2023-02-07 00:33:09 -0800

    Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import sys
-sys.path.append("/alx-higher_level_programming/0x02-python-import_modules/variable_load_5.py")
-import variable_load_5
+if __name__ == '__main__':
+    from variable_load_5 import a
 
 
-print(variable_load_5.a)
+    print(a)
