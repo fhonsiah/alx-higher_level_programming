34637d21785f72932264cdeac35f421c8fd45306
fhonsiah <fhonsiah@gmail.com>
2023-02-16 02:01:14 -0800

    Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, new_element):
+    """
+
+    A function replaces and element in a list at a specific position without
+    modifying the original list (like in C).
+
+    Args:
+        my_list: The list containing integers to be printed.
+        idx: The index/position of the element to be replaced.
+        new_element: The element to replace with.
+    """
+    if idx < 0:
+        return my_list[:]
+    elif idx > (len(my_list) - 1):
+        return my_list[:]
+    else:
+        my_list_copy = my_list[:]
+        my_list_copy[idx] = element
+        return my_list_copy
