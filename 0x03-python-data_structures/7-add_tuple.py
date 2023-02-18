ba0059c74e8db369961b75e33de37fc93226d01d
fhonsiah <fhonsiah@gmail.com>
2023-02-18 00:32:19 -0800

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b()):
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
-    return (a[0] + b[0], a[1] + b[1])
+    """
+
+    The function that adds 2 tuples.
+
+    Args:
+        tuple_a: The first tuple 
+        tuple_b: The seconf tuple
+    """
+    # Dealing with tuple_a
+    if not len(tuple_a):
+        tuple_a = 0, 0
+    elif len(tuple_a) == 1:
+        tuple_a = tuple_a[0], 0
+    # Dealing with tuple_b
+    if not len(tuple_b):
+        tuple_b = 0, 0
+    elif len(tuple_b) == 1:
+        tuple_b = tuple_b[0], 0
+
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
