ce4a12e15717fb44ea54fdecdadff6f3c9a6315a
fhonsiah <fhonsiah@gmail.com>
2023-02-18 09:34:58 -0800

    Delete at

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        # Index is out of range, do nothing and return the original list
+        return my_list
+    else:
+        # Slice the list to remove the item at the specified index
+        return my_list[:idx] + my_list[idx+1:]
