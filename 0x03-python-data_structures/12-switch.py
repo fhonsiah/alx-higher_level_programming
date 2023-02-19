9d9f7cb193e7f55e762fec8db34d2f9c41294704
fhonsiah <fhonsiah@gmail.com>
2023-02-19 02:03:17 -0800

    Switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -2,6 +2,9 @@
 a = 89
 b = 10
 
-# swap the values of a and b using a temporary variable
-a, b =  b, a
+# swap the values of a and b using arithmetic operations
+a = a + b
+b = a - b
+a = a - b
+
 print("a={:d} - b={:d}".format(a, b))
